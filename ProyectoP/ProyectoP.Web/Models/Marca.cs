66d568798d09473464483f473ed24ecbef8bcb32
Navi Dgz <navi0789@hotmail.com>
2020-05-30 20:50:02 -0700

    Modelos

---
+++ ProyectoP/ProyectoP.Web/Models/Marca.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoP.Web.Models
+{
+    public class Marca
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ProyectoP/ProyectoP.Web/Models/Perfume.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoP.Web.Models
+{
+    public class Perfume
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Gender { get; set; }
+        public decimal Price { get; set; }
+        public int MarcaId { get; set; }
+        [ForeignKey("MarcaId")]
+        public Marca Marca { get; set; }
+    }
+}
